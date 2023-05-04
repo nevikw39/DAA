5dae05d90f6f3882f429a3e61f9b2a13b78ac763
nevikw39 <nevikw39@icloud.com>
2023-05-05 03:33:41 +0800

    Update plot

--- Homework4/plot.py
+++ Homework4/plot.py
@@ -10,7 +10,7 @@ fig.suptitle("110062219")
 
 ax[0].axis('tight')
 ax[0].axis('off')
-ax[0].table(tuple((n, f"{bf:.1f}", f"{bb:.3f}") for n, bf, bb in zip(df['n'], df["bf"], df["bb"])), colLabels=("n", "Brute Force", "Branch & Bound"), edges="B", loc="center")
+ax[0].table(tuple((n, f"{bf:.1f}", f"{bb:.2f}") for n, bf, bb in zip(df['n'], df["bf"], df["bb"])), colLabels=("n", "Brute Force", "Branch & Bound"), edges="B", loc="center")
 
 ax[1].plot(df['n'], df["bf"], label="Brute Force")
 ax[1].plot(df['n'], df["bb"], label="Branch & Bound")
