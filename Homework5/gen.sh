c7c7258d5f95b8913439da6ca5d1175cdb9ab826
nevikw <nevikw@art2.inter.lsa>
2023-05-30 16:51:43 +0800

    Test on Please ask your administrator.

--- Homework5/gen.sh
+++ Homework5/gen.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 if [ ! -e data/$1.txt ]; then
-    echo $n $(($RANDOM % $1 + 1)) > data/$1.txt
+    echo $1 $(($RANDOM % $1 + 1)) > data/$1.txt
     for i in {1..$1}; do
         echo -n "$RANDOM " >> data/$1.txt
     done
